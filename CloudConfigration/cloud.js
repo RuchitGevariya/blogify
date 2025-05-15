4971d42066d7c52229a936bbd17ccbe48c3853cf
Ruchit Gevariya <ruchitgevariya@gmail.com>
2025-05-15 19:41:01 +0530

    changes on cloud config

--- CloudConfigration/cloud.js
+++ CloudConfigration/cloud.js
@@ -1,10 +1,14 @@
 
 import {v2 as cloudinary} from "cloudinary"
+// // that is way to config manule 
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,      
+//   api_secret: process.env.API_SECRET,
+// });
+// // that is way to config automatic that is when we have need to deploye your project 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,      
-  api_secret: process.env.API_SECRET,
-});
-
+  secure:true,
+})
 export default cloudinary
